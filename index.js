2eac1cdc40c912e5af214aaffd2610b27a7b85f4
beom-jun <chlrhwns1@naver.com>
2024-06-17 23:12:38 +0900

    js commit

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = navContainer.querySelectorAll('.nav2-index a');
     const dots = document.querySelectorAll('.dot-grid .dot');
     const searchIcon = document.querySelector('.search-icon');
-
+    const dropdownButton = document.querySelector('.nav1-earth');
+    const dropdownMenu = document.querySelector('.nav-hamburger-menu');
+    const navItems = document.querySelectorAll('.nav2-index a:not(.search-icon):not(.dot-grid)');
+    let navSearchIon = document.getElementById("search-icon-click")
+   
     // 네비게이션 컨테이너에 마우스 이벤트 추가
     navContainer.addEventListener('mouseenter', function() {
         navContainer.style.backgroundColor = 'white';
@@ -30,12 +34,47 @@ document.addEventListener('DOMContentLoaded', function() {
         searchIcon.classList.remove('blue');
     });
 
-    // 성결대학교 로고 이미지에 마우스 이벤트 추가
-    sungkyulImg.addEventListener('mouseenter', function() {
-        sungkyulImg.classList.add('blue');
+    // nav 언어버튼 메뉴 슬라이드
+    dropdownButton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
     });
 
-    sungkyulImg.addEventListener('mouseleave', function() {
-        sungkyulImg.classList.remove('blue');
+    document.addEventListener('click', function() {
+        dropdownMenu.style.display = 'none';
+    });
+
+    dropdownMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+
+    // nav 글씨에 마우스 올렸을 때 흰색 박스 세부 메뉴
+    navItems.forEach(item => {
+        const spanWidth = item.querySelector('span').offsetWidth;
+
+        item.addEventListener('mouseenter', function() {
+            const underline = item.querySelector('.underline'); 
+            if (underline) {
+                underline.style.width = `${spanWidth}px`; 
+            }
+            
+            const dropdown = this.querySelector('.dropdown-content');
+            if (dropdown) {
+                dropdown.style.display = 'block';
+            }
+        });
+
+        item.addEventListener('mouseleave', function() {
+            const dropdown = this.querySelector('.dropdown-content');
+            if (dropdown) {
+                dropdown.style.display = 'none';
+            }
+        });
     });
 });
+
+function searchClick(){
+    if(navSearchIon.style.display === "none"){
+        navSearchIon.style.display = "flex"
+    };
+}
